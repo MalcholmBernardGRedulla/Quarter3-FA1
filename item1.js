dd5df40154822aa93688b1fae52ec515dc3c070f
MalcholmBernardGRedulla <malcholmbernard.redulla@smc.pshs.edu.ph>
2025-01-29 18:11:57 +0800

    Update item1.js

--- item1.js
+++ item1.js
@@ -1,9 +1,12 @@
-var nick = "Mal";
-var h = 62;
-var w = 54;
+let nickname = "Mal";
+let heightInInches = 62;
+let weightInKg = 54;
 
-var hconv1 = h%12;
-var hconv2 = (h-hconv1)/12;
-var wconv = w*2.205;
+let heightInFeet = Math.floor(heightInInches / 12);
+let remainingInches = heightInInches % 12;
 
-alert("Name: " +nick+ "\nHeight: " +hconv2+ "'" +hconv1+ "'" + "\nWeight: " +wconv+ " lbs"); 
+let weightInPounds = (weightInKg * 2.20462).toFixed(2);
+
+let personalInfo = `Name: ${nickname}<br>Height: ${heightInFeet}'${remainingInches}"<br>Weight: ${weightInPounds} lbs`;
+
+alert(`Name: ${nickname}\nHeight: ${heightInFeet}'${remainingInches}"\nWeight: ${weightInPounds} lbs`);
