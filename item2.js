b11b2aecfb430c1609d8d30276be227339f2900b
MalcholmBernardGRedulla <malcholmbernard.redulla@smc.pshs.edu.ph>
2025-01-29 17:56:29 +0800

    Update item2.js

--- item2.js
+++ item2.js
@@ -1,5 +1,5 @@
 var FN = prompt("Type your first name: ");
-var LN = prompt("Type your Last name: ");
+var LN = prompt("Type your last name: ");
 var BY = parseInt(prompt("Type your birth year: "));
 
 document.getElementById("GFA").innerText="Hello " +FN+ " " +LN+"! How does it feel to be " +(2025-BY)+ " years old?";
