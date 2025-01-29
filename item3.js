119c9e60c2214e97f5aa4af1a2f985add650391e
MalcholmBernardGRedulla <malcholmbernard.redulla@smc.pshs.edu.ph>
2025-01-29 18:12:31 +0800

    Update item3.js

--- item3.js
+++ item3.js
@@ -1,14 +1,10 @@
-var nick = "Mal";
-var h = 62;
-var w = 54;
+let consent = confirm("Do you agree to share your personal information?");
 
-var hconv1 = h%12;
-var hconv2 = (h-hconv1)/12;
-var wconv = w*2.205;
-
-const shareInfo = confirm("Do you agree to share your personal information?");
-if (shareInfo) {
-  console.log("Name: " +nick+ "\nHeight: " +hconv2+ "'" +hconv1+ "'" + "\nWeight: " +wconv+ " lbs");
+if (consent) {
+    console.log(personalInfo);
+    document.write("<h2>Shared Information</h2>");
+    document.write(personalInfo);
 } else {
-  console.log("User does not wish to share his/her information.");
+    console.log("User does not wish to share his/her information.");
+    document.write("<h2>Consent Denied</h2><p>User does not wish to share his/her information.</p>");
 }
